7e9d23319711ebfb57dfc131d86db5beee3a44df
okhsunrog <me@okhsunrog.dev>
2025-05-05 18:03:04 +0300

    added mipidsi

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,31 +2,36 @@
 #![no_main]
 #![feature(allocator_api)]
 
+use core::alloc::Layout;
+
 use allocator_api2::{alloc::Allocator, vec::Vec};
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_alloc::HEAP;
 use esp_backtrace as _;
 use esp_hal::{
-    clock::CpuClock,
-    dma::{DmaRxBuf, DmaTxBuf, ExternalBurstConfig},
-    peripherals::DMA,
-    spi::{
-        Mode,
-        master::{Config, Spi},
-    },
-    time::Rate,
-    psram::{self, FlashFreq, PsramConfig, PsramSize, SpiRamFreq, SpiTimingConfigCoreClock},
-    timer::{systimer::SystemTimer, timg::TimerGroup},
+    clock::CpuClock, dma::{DmaRxBuf, DmaTxBuf, ExternalBurstConfig}, gpio::{Level, Output}, peripherals::DMA, psram::{self, FlashFreq, PsramConfig, PsramSize, SpiRamFreq, SpiTimingConfigCoreClock}, spi::{
+        master::{Config, Spi, SpiDmaBus}, Mode
+    }, time::Rate, timer::{systimer::SystemTimer, timg::TimerGroup}, Async
 };
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
+use static_cell::StaticCell;
+use embassy_sync::mutex::Mutex;
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use mipidsi::interface::SpiInterface;
+use mipidsi::{models::ST7789, options::ColorInversion, Builder};
 extern crate alloc;
 use log::{error, info};
 
 static PSRAM_ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
-const DMA_BUFFER_SIZE: usize = 8192;
+const DMA_BUFFER_SIZE: usize = 32000;
 const DMA_ALIGNMENT: ExternalBurstConfig = ExternalBurstConfig::Size64;
 const DMA_CHUNK_SIZE: usize = 4096 - DMA_ALIGNMENT as usize;
+
+// Display
+const W: i32 = 320;
+const H: i32 = 170;
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
@@ -73,7 +78,7 @@ async fn main(spawner: Spawner) {
     );
     let dma_tx_buf =
         DmaTxBuf::new_with_config(tx_descriptors, tx_buffer, DMA_ALIGNMENT).unwrap();
-    let (rx_buffer, rx_descriptors, _, _) = esp_hal::dma_buffers!(DMA_BUFFER_SIZE, 0);
+    let (rx_buffer, rx_descriptors, _, _) = esp_hal::dma_buffers!(4, 0);
     info!(
         "RX: {:p} len {} ({} descripters)",
         rx_buffer.as_ptr(),
@@ -91,7 +96,7 @@ async fn main(spawner: Spawner) {
 
     // Need to set miso first so that mosi can overwrite the
     // output connection (because we are using the same pin to loop back)
-    let mut spi = Spi::new(
+    let spi = Spi::new(
         p.SPI2,
         Config::default()
             .with_frequency(Rate::from_khz(100))
@@ -101,15 +106,31 @@ async fn main(spawner: Spawner) {
     .with_sck(sclk)
     .with_mosi(mosi)
     .with_dma(p.DMA_CH0)
-    .with_buffers(dma_rx_buf, dma_tx_buf);
+    .with_buffers(dma_rx_buf, dma_tx_buf)
+    .into_async();
+
+    let disp_buffer = unsafe {PSRAM_ALLOCATOR.allocate(Layout::from_size_align(1024, 1).unwrap()).unwrap().as_mut()};
+    let res = Output::new(res, Level::Low, Default::default());
+    let dc = Output::new(dc, Level::Low, Default::default());
+    let cs = Output::new(cs, Level::High, Default::default());
+    static SPI_BUS: StaticCell<Mutex<NoopRawMutex, SpiDmaBus<'static, Async>>> = StaticCell::new();
+    let spi_bus = Mutex::new(spi);
+    let spi_bus = SPI_BUS.init(spi_bus);
+    let spi_device = SpiDevice::new(spi_bus, cs);
+    let di = SpiInterface::new(spi_device, dc, disp_buffer);
+    let mut delay = embassy_time::Delay;
+    let display = Builder::new(ST7789, di).display_size(W as u16, H as u16)
+    .invert_colors(ColorInversion::Inverted)
+    .reset_pin(res)
+    .init(&mut delay)
+    .await
+    .unwrap();
+
     
 
     // TODO: Spawn some tasks
     let _ = spawner;
 
-    // Testing heaps
-    let large_buffer: Vec<u8, _> = Vec::with_capacity_in(1048576, &PSRAM_ALLOCATOR);
-    let small_buffer: Vec<u8, _> = Vec::with_capacity_in(1024, &HEAP);
     info!("Global heap stats: {}", HEAP.stats());
     info!("PSRAM heap stats: {}", PSRAM_ALLOCATOR.stats());
 
