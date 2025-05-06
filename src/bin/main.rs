72f16b40db20ecd456892d2b39a500e417c43044
okhsunrog <me@okhsunrog.dev>
2025-05-06 17:48:16 +0300

    dma, higher clock

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,7 +8,7 @@ use alloc::{alloc::alloc_zeroed, vec};
 use allocator_api2::{alloc::Allocator, boxed::Box, vec::Vec};
 use embassy_executor::Spawner;
 use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-use embassy_time::{Duration, Timer};
+use embassy_time::{Duration, Instant, Timer};
 use embedded_hal_bus::spi::ExclusiveDevice;
 use esp_alloc::{EspHeap, HEAP};
 use esp_backtrace as _;
@@ -46,6 +46,10 @@ use log::{error, info};
 
 static PSRAM_ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
+const DMA_BUFFER_SIZE: usize = 32000;
+const DMA_ALIGNMENT: ExternalBurstConfig = ExternalBurstConfig::Size64;
+const DMA_CHUNK_SIZE: usize = 4096 - DMA_ALIGNMENT as usize;
+
 // Display
 // Size
 const W: u16 = 205;
@@ -54,8 +58,8 @@ const H: u16 = 320;
 const X_OFFSET: u16 = 35;
 const Y_OFFSET: u16 = 0;
 // Active area
-const W_ACTIVE: u16 = W - X_OFFSET;
-const H_ACTIVE: u16 = H - Y_OFFSET;
+const W_ACTIVE: u16 = W - X_OFFSET; //170
+const H_ACTIVE: u16 = H - Y_OFFSET; //320
 
 #[esp_hal_embassy::main]
 async fn main(_spawner: Spawner) {
@@ -82,6 +86,28 @@ async fn main(_spawner: Spawner) {
 
     info!("Embassy initialized!");
 
+    // DMA for SPI
+    let (_, tx_descriptors) =
+        esp_hal::dma_descriptors_chunk_size!(0, DMA_BUFFER_SIZE, DMA_CHUNK_SIZE);
+    let layout =
+        core::alloc::Layout::from_size_align(DMA_BUFFER_SIZE, DMA_ALIGNMENT as usize).unwrap();
+    let tx_buffer: &mut [u8] = unsafe { PSRAM_ALLOCATOR.allocate(layout).unwrap().as_mut() };
+    // info!(
+    //     "TX: {:p} len {} ({} descripters)",
+    //     tx_buffer.as_ptr(),
+    //     tx_buffer.len(),
+    //     tx_descriptors.len()
+    // );
+    let dma_tx_buf = DmaTxBuf::new_with_config(tx_descriptors, tx_buffer, DMA_ALIGNMENT).unwrap();
+    let (rx_buffer, rx_descriptors, _, _) = esp_hal::dma_buffers!(4, 0);
+    // info!(
+    //     "RX: {:p} len {} ({} descripters)",
+    //     rx_buffer.as_ptr(),
+    //     rx_buffer.len(),
+    //     rx_descriptors.len()
+    // );
+    let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+
     // Creating SPI
     let sclk = p.GPIO4;
     let mosi = p.GPIO5;
@@ -91,12 +117,14 @@ async fn main(_spawner: Spawner) {
     let spi = Spi::new(
         p.SPI2,
         Config::default()
-            .with_frequency(Rate::from_mhz(40))
+            .with_frequency(Rate::from_mhz(80))
             .with_mode(Mode::_0),
     )
     .unwrap()
     .with_sck(sclk)
-    .with_mosi(mosi);
+    .with_mosi(mosi)
+    .with_dma(p.DMA_CH0)
+    .with_buffers(dma_rx_buf, dma_tx_buf);
 
     let mut disp_buffer: Vec<u8, &EspHeap> = Vec::new_in(&PSRAM_ALLOCATOR);
     disp_buffer.resize(W_ACTIVE as usize * H_ACTIVE as usize * 2, 0);
@@ -120,6 +148,7 @@ async fn main(_spawner: Spawner) {
         .unwrap();
     info!("Display initialized!");
 
+    let start = Instant::now();
     display
         .fill_solid(
             &Rectangle {
@@ -132,8 +161,14 @@ async fn main(_spawner: Spawner) {
             Rgb565::BLACK,
         )
         .unwrap();
+    let elapsed = start.elapsed();
+    info!("Filled and sent in {}us", elapsed.as_micros());
     Timer::after(Duration::from_secs(1)).await;
+
+    let start = Instant::now();
     TestImage::new().draw(&mut display).unwrap();
+    let elapsed = start.elapsed();
+    info!("Drew image in {}us", elapsed.as_micros());
 
     //info!("Global heap stats: {}", HEAP.stats());
     info!("PSRAM heap stats: {}", PSRAM_ALLOCATOR.stats());
